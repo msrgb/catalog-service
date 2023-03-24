ff19a775caa74336041d6e69918c5370142f5d08
Ramgopalbhat10 <ramgopalbhat10@gmail.com>
2023-03-24 05:34:43 +0000

    added config client

--- src/main/java/com/mrgb/catalogservice/HomeController.java
+++ src/main/java/com/mrgb/catalogservice/HomeController.java
@@ -3,11 +3,19 @@ package com.mrgb.catalogservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mrgb.catalogservice.config.PolarProperties;
+
 @RestController
 public class HomeController {
 
+  private final PolarProperties polarProperties;
+
+  public HomeController(PolarProperties polarProperties) {
+    this.polarProperties = polarProperties;
+  }
+
   @GetMapping("/")
   public String getGreeting() {
-    return "Welcome to the book catalog!";
+    return polarProperties.getGreeting();
   }
 }
